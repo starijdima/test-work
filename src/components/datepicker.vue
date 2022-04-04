9b5029a9b1d8d5e656b5597af8cd70d333e5f7ef
starijdmitry <starijdmitry@yandex.ru>
2022-04-04 17:31:53 +0300

    add calendar logic

--- src/components/datepicker.vue
+++ src/components/datepicker.vue
@@ -1,11 +1,81 @@
 <template>
-  <div>
-
+  <div class="calendar">
+    <div class="header z-depth-2">
+      <a @click="lastMonth" class="waves-effect waves-light btn"><i class="material-icons left">chevron_left</i>Last Month</a>
+      <p>{{month}} {{year}}</p>
+      <a @click="nextMonth" class="waves-effect waves-light btn"><i class="material-icons right">chevron_right</i>Next Month</a>
+    </div>
+    <ul class="dates month">
+      <li class="dow" v-for="dow in days">{{dow}}</li>
+      <li v-for="blank in firstDayOfMonth" class="day"></li>
+      <li v-for="date in daysInMonth" @click="openday(date)"
+          class="day" :class="{'today': date == initialDate && month == initialMonth && year == initialYear}">
+        <span>{{date}}</span>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
+import moment from 'moment'
 export default {
-  name: "datepicker"
+  props: {
+    events: []
+  },
+  name: "datepicker",
+  data(){
+    return{
+      today: this.moment(),
+      dateContext: this.moment(),
+      days: ['S', 'M', 'T', 'W', 'T', 'F', 'S']
+    }
+  },
+  methods:{
+    moment:moment,
+    nextMonth: function () {
+      let t = this;
+      t.dateContext = moment(t.dateContext).add(1, 'month');
+    },
+    lastMonth: function () {
+      let t = this;
+      t.dateContext = moment(t.dateContext).subtract(1, 'month');
+    }
+  },
+  computed: {
+    year: function () {
+      let t = this;
+      return t.dateContext.format('YYYY');
+    },
+    month: function () {
+      let t = this;
+      return t.dateContext.format('MMMM');
+    },
+    daysInMonth: function () {
+      let t = this;
+      return t.dateContext.daysInMonth();
+    },
+    currentDate: function () {
+      let t = this;
+      return t.dateContext.get('date');
+    },
+    firstDayOfMonth: function () {
+      let t = this;
+      let firstDay = moment(t.dateContext).subtract((t.currentDate - 1), 'days');
+      return firstDay.weekday();
+    },
+    //Previous Code Above
+    initialDate: function () {
+      let t = this;
+      return t.today.get('date');
+    },
+    initialMonth: function () {
+      let t = this;
+      return t.today.format('MMMM');
+    },
+    initialYear: function () {
+      let t = this;
+      return t.today.format('YYYY');
+    }
+  }
 }
 </script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <datepicker></datepicker>
+    <datepicker v-bind:events="events"></datepicker>
   </div>
 </template>
 
@@ -10,6 +10,15 @@ export default {
   name: 'HomeView',
   components: {
     datepicker
-  }
+  },
+  data(){
+    return{
+      events: [
+        {id: 0, name: 'test-1', date: '123', type: 1},
+        {id: 1, name: 'test-2', date: '123', type: 2},
+        {id: 2, name: 'test-3', date: '123', type: 3}
+      ]
+    }
+  },
 }
 </script>
